5e9a8a91865c55df0bf9fa616f28b7e91c6819e6
Javith farkhan. S <94296805+Javith-farkhan@users.noreply.github.com>
2023-05-28 19:32:14 +0530

    Add files via upload

---
+++ Server_5.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import socket
+s=socket.socket()
+s.connect(('localhost',8000))
+while True:
+    ip=input("Enter MAC Address : ")
+    s.send(ip.encode())
+    print("Logical Address",s.recv(1024).decode())
+
+
+# In[ ]:
+
+
+
+
